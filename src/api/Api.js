67825b468cf50ca99e6a3c49e581efbe0f971458
Igor Tuag <igortuag@gmail.com>
2021-01-04 08:22:05 -0400

    create tokenPost

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import TokenPost from './endpoints/TokenPost'
 import UserPost from './endpoints/UserPost'
 
 const Api = () => {
   return <div>
     <h2>USER POST</h2>
     <UserPost />
+    <h2>TOKEN POST</h2>
+    <TokenPost />
   </div>
 }
 
