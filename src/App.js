77e34c6804060409981d35d8901238de9f1c5d19
Igor Tuag <igortuag@gmail.com>
2021-01-18 19:50:20 -0300

    Add initial structure to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import Api from './api/Api'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Login from "./components/Login/Login";
 
 function App() {
-  return <div><Api/></div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
