0734faa3e3479f71d099724d366757f8cfdeaa53
Tuag <igortuag@gmail.com>
2022-03-10 07:00:09 -0300

    add styles to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import { ReactComponent as Dogs } from "../Assets/dogs.svg";
 
 const Header = () => {
   return (
-    <div className={styles.header}>
+    <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
         <Link className={styles.logo} to="/" aria-label="Dogs - Home">
           <Dogs />
@@ -15,7 +15,7 @@ const Header = () => {
           Login / Criar
         </Link>
       </nav>
-    </div>
+    </header>
   );
 };
 
