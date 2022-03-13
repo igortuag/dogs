ff23f029e6182848d9d12192226e56b756714b47
Tuag <igortuag@gmail.com>
2022-03-13 06:31:52 -0300

    verify error and display if exists

--- src/components/Forms/Input.js
+++ src/components/Forms/Input.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import styles from "./Input.module.css";
 
-const Input = ({ label, type, name, value, onChange }) => {
+const Input = ({ label, type, name, value, onChange, error }) => {
   return (
     <div className={styles.wrapper}>
       <label htmlFor={name} className={styles.label}>
@@ -16,7 +16,7 @@ const Input = ({ label, type, name, value, onChange }) => {
         value={value}
         onChange={onChange}
       />
-      <p className={styles.error}>Error</p>
+      {error && <p className={styles.error}>{error}</p>}
     </div>
   );
 };
