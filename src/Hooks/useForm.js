8ecf0a87604279c8865f720938943dca8e840b6d
Tuag <igortuag@gmail.com>
2022-03-13 06:20:43 -0300

    add function on change

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -3,9 +3,14 @@ import React from "react";
 const useForm = () => {
   const [value, setValue] = React.useState("");
 
+  function onChange({ target }) {
+    setValue(target.value);
+  }
+
   return {
     value,
     setValue,
+    onChange,
   };
 };
 
