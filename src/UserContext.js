d4d0bed1012bc7f05c4c35923e24055ef94dde41
Tuag <igortuag@gmail.com>
2022-03-15 06:58:06 -0300

    add variables to userContext

--- src/UserContext.js
+++ src/UserContext.js
@@ -3,6 +3,11 @@ import React from "react";
 export const UserContext = React.createContext();
 
 export const UserStorage = ({ children }) => {
+  const [data, setData] = React.useState(null);
+  const [login, setLogin] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
+
   return (
     <UserContext.Provider value={{ user: "Tuag" }}>
       {children}
