c12b68e4529cba11dce3b5b3c843a464b42d9ce9
Tuag <igortuag@gmail.com>
2022-03-15 06:50:41 -0300

    add children to context

--- src/UserContext.js
+++ src/UserContext.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export const UserContext = React.createContext();
 
-export const UserStorage = () => {
-  return <UserContext.Provider>UserStorage</UserContext.Provider>;
+export const UserStorage = ({ children }) => {
+  return <UserContext.Provider>{children}</UserContext.Provider>;
 };
