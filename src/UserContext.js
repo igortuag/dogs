a28381e863b472e1e7c4c9b15f4e5792c3ad0378
Tuag <igortuag@gmail.com>
2022-03-15 07:01:00 -0300

    add method to getUser

--- src/UserContext.js
+++ src/UserContext.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { TOKEN_POST } from "./api";
+import { TOKEN_POST, USER_GET } from "./api";
 
 export const UserContext = React.createContext();
 
@@ -8,6 +8,15 @@ export const UserStorage = ({ children }) => {
   const [login, setLogin] = React.useState(null);
   const [loading, setLoading] = React.useState(false);
   const [error, setError] = React.useState(null);
+
+  async function getUser(token) {
+    const { url, options } = USER_GET(token);
+
+    const response = await fetch(url, options);
+    const json = await response.json();
+
+    setData(json);
+  }
 
   async function userLogin(username, password) {
     const { url, options } = TOKEN_POST({
@@ -19,7 +28,7 @@ export const UserStorage = ({ children }) => {
     const { token } = await response.json();
 
     window.localStorage.setItem("token", token);
-    // getUser(token);
+    getUser(token);
   }
 
   return (
