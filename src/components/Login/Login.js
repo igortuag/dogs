5bdac78e69fece12b566ed6610a5b1acfe2057ea
Tuag <igortuag@gmail.com>
2022-03-18 06:49:30 -0300

    add styles to login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,6 +6,7 @@ import LoginCreated from "./LoginCreated";
 import LoginForm from "./LoginForm";
 import LoginLostPassword from "./LoginLostPassword";
 import LoginResetPassword from "./LoginResetPassword";
+import styles from "./Login.module.css";
 
 const Login = () => {
   const { login } = React.useContext(UserContext);
@@ -15,13 +16,15 @@ const Login = () => {
   }
 
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="create" element={<LoginCreated />} />
-        <Route path="lost-password" element={<LoginLostPassword />} />
-        <Route path="reset-password" element={<LoginResetPassword />} />
-      </Routes>
+    <div className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="create" element={<LoginCreated />} />
+          <Route path="lost-password" element={<LoginLostPassword />} />
+          <Route path="reset-password" element={<LoginResetPassword />} />
+        </Routes>
+      </div>
     </div>
   );
 };
