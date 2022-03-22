a024c35ae021b81ec55c3b8b0b32ef91a550c05e
Igor Tuag <igortuag@gmail.com>
2022-03-22 06:56:26 -0300

    fix protected routing method

--- src/components/Helper/ProtectedRoute.js
+++ src/components/Helper/ProtectedRoute.js
@@ -1,17 +1,13 @@
 import React from "react";
-import { Navigate, Route } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import { UserContext } from "../../UserContext";
 
-const ProtectedRoute = (props) => {
+const ProtectedRoute = () => {
   const { login } = React.useContext(UserContext);
 
-  if (login) {
-    return <Route to="/user" {...props} />;
-  } else if (login === false) {
-    return <Navigate to="/user" />;
-  }
-
-  return null;
+  if (login) return <Outlet />;
+  else if (login === false) return <Navigate to="/login" />;
+  else return null;
 };
 
 export default ProtectedRoute;
