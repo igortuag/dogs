956bed39910e6e9104a25bb707d9256bb73528fe
Igor Tuag <igortuag@gmail.com>
2022-03-23 08:00:35 -0300

    add action to logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import { ReactComponent as Dogs } from "../Assets/dogs.svg";
 import { UserContext } from "../UserContext";
 
 const Header = () => {
-  const { data, userLogout } = React.useContext(UserContext);
+  const { data } = React.useContext(UserContext);
 
   return (
     <header className={styles.header}>
@@ -19,7 +19,6 @@ const Header = () => {
             <Link className={styles.login} to="/user">
               {data.nome}
             </Link>
-            <button onClick={userLogout}>Sair</button>
           </>
         ) : (
           <Link className={styles.login} to="/login">
--- src/components/User/UserHeaderNav.js
+++ src/components/User/UserHeaderNav.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { UserContext } from "../../UserContext";
 
 const UserHeaderNav = () => {
+  const { userLogout } = React.useContext(UserContext);
+
   return (
     <nav>
       <NavLink to="/user">Minhas Fotos</NavLink>
       <NavLink to="/user/stats">Estatísticas</NavLink>
       <NavLink to="/user/post">Adicionar Foto</NavLink>
-      <button>Sair</button>
+      <button onClick={userLogout}>Sair</button>
     </nav>
   );
 };
