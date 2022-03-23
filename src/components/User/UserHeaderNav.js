42889943e07513f09aed5dcaf3599996989ca72f
Igor Tuag <igortuag@gmail.com>
2022-03-23 08:04:07 -0300

    add icons to actions

--- src/components/User/UserHeaderNav.js
+++ src/components/User/UserHeaderNav.js
@@ -1,16 +1,31 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { UserContext } from "../../UserContext";
+import { ReactComponent as Feed } from "../../Assets/feed.svg";
+import { ReactComponent as Stats } from "../../Assets/estatisticas.svg";
+import { ReactComponent as Add } from "../../Assets/adicionar.svg";
+import { ReactComponent as Logout } from "../../Assets/sair.svg";
 
 const UserHeaderNav = () => {
   const { userLogout } = React.useContext(UserContext);
 
   return (
     <nav>
-      <NavLink to="/user">Minhas Fotos</NavLink>
-      <NavLink to="/user/stats">Estatísticas</NavLink>
-      <NavLink to="/user/post">Adicionar Foto</NavLink>
-      <button onClick={userLogout}>Sair</button>
+      <NavLink to="/user">
+        <Feed /> Minhas Fotos
+      </NavLink>
+      <NavLink to="/user/stats">
+        <Stats />
+        Estatísticas
+      </NavLink>
+      <NavLink to="/user/post">
+        <Add />
+        Adicionar Foto
+      </NavLink>
+      <button onClick={userLogout}>
+        <Logout />
+        Sair
+      </button>
     </nav>
   );
 };
