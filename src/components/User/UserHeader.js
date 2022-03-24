d0993258e4e00a3d4c0fe1c4c1d0f498d7ec458b
Igor Tuag <igortuag@gmail.com>
2022-03-24 07:31:22 -0300

    add dynamic title

--- src/components/User/UserHeader.js
+++ src/components/User/UserHeader.js
@@ -2,11 +2,31 @@ import React from "react";
 
 import UserHeaderNav from "./UserHeaderNav";
 import styles from "./UserHeader.module.css";
+import { useLocation } from "react-router-dom";
 
 const UserHeader = () => {
+  const [title, setTitle] = React.useState("");
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    switch (pathname) {
+      case "/user":
+        setTitle("Minha Conta");
+        break;
+      case "/user/post":
+        setTitle("Poste sua foto");
+        break;
+      case "/user/stats":
+        setTitle("Estatísticas");
+        break;
+      default:
+        setTitle("");
+    }
+  }, [pathname]);
+
   return (
     <section className={styles.header}>
-      <h1 className="title">Título</h1>
+      <h1 className="title">{title}</h1>
       <UserHeaderNav />
     </section>
   );
