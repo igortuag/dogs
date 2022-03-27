ddc1694d1276c45271692ff4b6d09e8ded26f776
Igor Tuag <igortuag@gmail.com>
2022-03-27 08:45:29 -0300

    add state to form

--- src/components/User/UserPhotoPost.js
+++ src/components/User/UserPhotoPost.js
@@ -2,22 +2,28 @@ import React from "react";
 import styles from "./UserPhotoPost.module.css";
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
+import useForm from "../../Hooks/useForm";
 
 const UserPhotoPost = () => {
+  const nome = useForm();
+  const peso = useForm("number");
+  const idade = useForm("number");
+  const [img, setImg] = React.useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
   };
 
   const handleImgChange = (e) => {
-    const img = e.target.files[0];
+    setImg(e.target.files[0]);
   };
 
   return (
     <section className={`${styles.photoPost} animeLeft`}>
       <form onSubmit={handleSubmit}>
-        <Input label="Nome" type="text" name="nome" />
-        <Input label="Peso" type="text" name="peso" />
-        <Input label="Idade" type="text" name="idade" />
+        <Input label="Nome" type="text" name="nome" {...nome} />
+        <Input label="Peso" type="text" name="peso" {...peso} />
+        <Input label="Idade" type="text" name="idade" {...idade} />
         <input type="file" name="img" id="img" onChange={handleImgChange} />
         <Button>Enviar</Button>
       </form>
