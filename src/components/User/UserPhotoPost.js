69674727f3e2322b969352cb04b0fa61f6352a6c
Igor Tuag <igortuag@gmail.com>
2022-03-27 08:42:56 -0300

    add structure to page post photo

--- src/components/User/UserPhotoPost.js
+++ src/components/User/UserPhotoPost.js
@@ -1,10 +1,27 @@
 import React from "react";
+import styles from "./UserPhotoPost.module.css";
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
 
 const UserPhotoPost = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleImgChange = (e) => {
+    const img = e.target.files[0];
+  };
+
   return (
-    <div>
-      <h1>UserPhotoPost</h1>
-    </div>
+    <section className={`${styles.photoPost} animeLeft`}>
+      <form onSubmit={handleSubmit}>
+        <Input label="Nome" type="text" name="nome" />
+        <Input label="Peso" type="text" name="peso" />
+        <Input label="Idade" type="text" name="idade" />
+        <input type="file" name="img" id="img" onChange={handleImgChange} />
+        <Button>Enviar</Button>
+      </form>
+    </section>
   );
 };
 
