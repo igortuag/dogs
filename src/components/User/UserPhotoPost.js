9f8cc0f29ed6c17609e9404e104e85f086b908ec
Tuag <igortuag@gmail.com>
2022-03-28 09:23:20 -0300

    add efect on submit form

--- src/components/User/UserPhotoPost.js
+++ src/components/User/UserPhotoPost.js
@@ -6,6 +6,7 @@ import useForm from "../../Hooks/useForm";
 import useFetch from "../../Hooks/useFetch";
 import { PHOTO_POST } from "../../api";
 import Error from "../Helper/Error";
+import { useNavigate } from "react-router-dom";
 
 const UserPhotoPost = () => {
   const nome = useForm();
@@ -13,6 +14,13 @@ const UserPhotoPost = () => {
   const idade = useForm("number");
   const [img, setImg] = React.useState("");
   const { data, error, loading, request } = useFetch();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    if (data) {
+      navigate("/user");
+    }
+  }, [data, navigate]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
