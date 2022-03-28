2ab227f76b2dc56e832d3394be0bb9509f4648f8
Tuag <igortuag@gmail.com>
2022-03-28 09:16:12 -0300

    add styles to file input

--- src/components/User/UserPhotoPost.js
+++ src/components/User/UserPhotoPost.js
@@ -40,7 +40,13 @@ const UserPhotoPost = () => {
         <Input label="Nome" type="text" name="nome" {...nome} />
         <Input label="Peso" type="number" name="peso" {...peso} />
         <Input label="Idade" type="number" name="idade" {...idade} />
-        <input type="file" name="img" id="img" onChange={handleImgChange} />
+        <input
+          className={styles.file}
+          type="file"
+          name="img"
+          id="img"
+          onChange={handleImgChange}
+        />
         <Button>Enviar</Button>
       </form>
       <div>
