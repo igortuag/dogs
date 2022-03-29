2a9ba6816d3d3837d53a3e1bd2fa9da3cc6da4c5
Tuag <igortuag@gmail.com>
2022-03-29 08:01:43 -0300

    add method to fetchPhotos

--- src/components/Feed/FeedPhotos.js
+++ src/components/Feed/FeedPhotos.js
@@ -1,7 +1,26 @@
 import React from "react";
 import FeedPhotosItem from "./FeedPhotosItem";
+import useFetch from "../../Hooks/useFetch";
+import { PHOTOS_GET } from "../../api";
 
 function FeedPhotos() {
+  const { data, loading, error, request } = useFetch();
+
+  React.useEffect(() => {
+    async function fetchPhotos() {
+      const { url, options } = PHOTOS_GET({
+        page: 1,
+        total: 6,
+        user: 0,
+      });
+
+      const { response, json } = await request(url, options);
+
+      console.log("response ::>>", response);
+    }
+    fetchPhotos();
+  }, [request]);
+
   return <FeedPhotosItem />;
 }
 
