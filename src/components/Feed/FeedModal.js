777ef2c0081872a566a9495bad2d74ee8e5fbb74
Tuag <igortuag@gmail.com>
2022-03-31 07:36:41 -0300

    add fetching to photo on modal

--- src/components/Feed/FeedModal.js
+++ src/components/Feed/FeedModal.js
@@ -1,7 +1,17 @@
 import React from "react";
 import styles from "./FeedModal.module.css";
+import useFetch from "../../hooks/useFetch";
+import { PHOTO_GET } from "../../api";
 
-function FeedModal() {
+function FeedModal({ photo }) {
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    const { url, options } = PHOTO_GET(photo?.id);
+
+    request(url, options);
+  }, [photo, request]);
+
   return <div className={styles.modal}>FeedModal</div>;
 }
 
