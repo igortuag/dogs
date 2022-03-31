6370d3dca69b4699fe796ce4709412b61ac5d51a
Tuag <igortuag@gmail.com>
2022-03-31 07:43:46 -0300

    add new component to photo content

--- src/components/Feed/FeedModal.js
+++ src/components/Feed/FeedModal.js
@@ -1,7 +1,10 @@
 import React from "react";
 import styles from "./FeedModal.module.css";
 import useFetch from "../../Hooks/useFetch";
+import Error from "../Helper/Error";
+import Loading from "../Helper/Loading";
 import { PHOTO_GET } from "../../api";
+import PhotoContent from "../Photo/PhotoContent";
 
 function FeedModal({ photo }) {
   const { data, error, loading, request } = useFetch();
@@ -14,7 +17,9 @@ function FeedModal({ photo }) {
 
   return (
     <div className={styles.modal}>
-      <img src={photo.src} alt="" />
+      {error && <Error error={error} />}
+      {loading && <Loading />}
+      {data && <PhotoContent data={data} />}
     </div>
   );
 }
---
+++ src/components/Photo/PhotoContent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PhotoContent() {
+  return <div>PhotoContent</div>;
+}
+
+export default PhotoContent;
