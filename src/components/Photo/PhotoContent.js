ee56054951ad86bb8fad9e6f73bd1ff5ea174188
Tuag <igortuag@gmail.com>
2022-03-31 07:48:24 -0300

    add photo info

--- src/components/Photo/PhotoContent.js
+++ src/components/Photo/PhotoContent.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./PhotoContent.module.css";
 
-function PhotoContent() {
-  return <div>PhotoContent</div>;
+function PhotoContent({ data }) {
+  const { photo, comments } = data;
+
+  return (
+    <div className={styles.photo}>
+      <div className={styles.img}>
+        <img src={photo.src} alt={photo.title} />
+      </div>
+      <div className={styles.details}>
+        <div>
+          <p>
+            <Link to={`/profile/${photo.author}`}>@{photo.author}</Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PhotoContent;
