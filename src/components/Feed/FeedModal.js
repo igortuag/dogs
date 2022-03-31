ebdf03eeab7845a18e4179a1326808792033da68
Tuag <igortuag@gmail.com>
2022-03-31 07:41:10 -0300

    display photo on click

--- src/components/Feed/FeedModal.js
+++ src/components/Feed/FeedModal.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./FeedModal.module.css";
-import useFetch from "../../hooks/useFetch";
+import useFetch from "../../Hooks/useFetch";
 import { PHOTO_GET } from "../../api";
 
 function FeedModal({ photo }) {
@@ -12,7 +12,11 @@ function FeedModal({ photo }) {
     request(url, options);
   }, [photo, request]);
 
-  return <div className={styles.modal}>FeedModal</div>;
+  return (
+    <div className={styles.modal}>
+      <img src={photo.src} alt="" />
+    </div>
+  );
 }
 
 export default FeedModal;
--- src/components/Feed/FeedPhotosItem.js
+++ src/components/Feed/FeedPhotosItem.js
@@ -4,7 +4,7 @@ import styles from "./FeedPhotosItem.module.css";
 
 function FeedPhotosItem({ photo, setModalPhoto }) {
   return (
-    <li className={styles.photo} onClick={() => setModalPhoto(photo.id)}>
+    <li className={styles.photo} onClick={() => setModalPhoto(photo)}>
       <img src={photo.src} alt={photo.title} />
       <span className={styles.view}>{photo.acessos}</span>
     </li>
