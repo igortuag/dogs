711880e909f8e283e59bae1fe57d28ba28b95c7c
Tuag <igortuag@gmail.com>
2022-04-02 08:38:31 -0300

    add photo comments form

--- src/components/Photo/PhotoComments.js
+++ src/components/Photo/PhotoComments.js
@@ -1,7 +1,11 @@
 import React from "react";
+import UserContext from "../../UserContext";
+import PhotoCommentsForm from "./PhotoCommentsForm";
 
 function PhotoComments() {
-  return <div>PhotoComments</div>;
+  const { login } = React.useContext(UserContext);
+
+  return <div>{login && <PhotoCommentsForm />}</div>;
 }
 
 export default PhotoComments;
---
+++ src/components/Photo/PhotoCommentsForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PhotoCommentsForm() {
+  return <div>PhotoCommentsForm</div>;
+}
+
+export default PhotoCommentsForm;
