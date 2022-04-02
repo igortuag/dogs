e9f852754fb97b108a7626a7278ba45d834568b6
Tuag <igortuag@gmail.com>
2022-04-02 08:49:10 -0300

    add list of comments

--- src/components/Photo/PhotoComments.js
+++ src/components/Photo/PhotoComments.js
@@ -2,10 +2,23 @@ import React from "react";
 import UserContext from "../../UserContext";
 import PhotoCommentsForm from "./PhotoCommentsForm";
 
-function PhotoComments({ id }) {
+function PhotoComments({ id, comments }) {
+  const [comments, setComments] = React.useState(comments);
   const { login } = React.useContext(UserContext);
 
-  return <div>{login && <PhotoCommentsForm id={id} />}</div>;
+  return (
+    <div>
+      <ul>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}: </b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentsForm id={id} />}
+    </div>
+  );
 }
 
 export default PhotoComments;
