393ddb202d7f6a478d7edbb92f05502f2dd8834e
Tuag <igortuag@gmail.com>
2022-04-02 08:40:43 -0300

    add comment input

--- src/components/Photo/PhotoComments.js
+++ src/components/Photo/PhotoComments.js
@@ -2,10 +2,10 @@ import React from "react";
 import UserContext from "../../UserContext";
 import PhotoCommentsForm from "./PhotoCommentsForm";
 
-function PhotoComments() {
+function PhotoComments({ id }) {
   const { login } = React.useContext(UserContext);
 
-  return <div>{login && <PhotoCommentsForm />}</div>;
+  return <div>{login && <PhotoCommentsForm id={id} />}</div>;
 }
 
 export default PhotoComments;
--- src/components/Photo/PhotoCommentsForm.js
+++ src/components/Photo/PhotoCommentsForm.js
@@ -1,7 +1,18 @@
 import React from "react";
 
-function PhotoCommentsForm() {
-  return <div>PhotoCommentsForm</div>;
+function PhotoCommentsForm({ id }) {
+  const [comment, setComment] = React.useState("");
+
+  return (
+    <form>
+      <textarea
+        name="comment"
+        id="comment"
+        value={comment}
+        onChange={({ target }) => setComment(target.value)}
+      />
+    </form>
+  );
 }
 
 export default PhotoCommentsForm;
