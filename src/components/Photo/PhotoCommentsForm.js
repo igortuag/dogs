8f4bc5dd12dfcafa7c8ca6eebeb73df854eb92a6
Tuag <igortuag@gmail.com>
2022-04-02 08:46:14 -0300

    add method to send comments

--- src/components/Photo/PhotoCommentsForm.js
+++ src/components/Photo/PhotoCommentsForm.js
@@ -1,14 +1,26 @@
 import React from "react";
+import { COMMENT_POST } from "../../api";
 import { ReactComponent as Enviar } from "../../Assets/enviar.svg";
+import useFetch from "../../Hooks/useFetch";
 
 function PhotoCommentsForm({ id }) {
+  const { request, error } = useFetch();
   const [comment, setComment] = React.useState("");
 
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const { url, options } = COMMENT_POST(id, { comment });
+
+    await request(url, options);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <textarea
         name="comment"
         id="comment"
+        placeholder="Comente..."
         value={comment}
         onChange={({ target }) => setComment(target.value)}
       />
