aff5f121eed1ccd5b8b3855226285cc87fe06ed5
Tuag <igortuag@gmail.com>
2022-04-04 07:23:13 -0300

    add api methodo to delete photo

--- src/api.js
+++ src/api.js
@@ -96,3 +96,15 @@ export function COMMENT_POST(id, body) {
     body: JSON.stringify(body),
   };
 }
+
+export function PHOTO_DELETE(id) {
+  return {
+    url: `${API_URL}/api/photo/${id}`,
+    options: {
+      method: "DELETE",
+      headers: {
+        Authorization: "Bearer " + window.localStorage.getItem("token"),
+      },
+    },
+  };
+}
