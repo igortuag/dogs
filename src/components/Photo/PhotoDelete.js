201fdc09a81bc9739c398905491f813f29b4f97f
Tuag <igortuag@gmail.com>
2022-04-04 07:25:49 -0300

    add method to delete photo

--- src/components/Photo/PhotoDelete.js
+++ src/components/Photo/PhotoDelete.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { PHOTO_DELETE } from "../../api";
+import useFetch from "../../Hooks/useFetch";
 import styles from "./PhotoComments.module.css";
 
 function PhotoDelete({ id }) {
+  const { loading, request } = useFetch();
+
+  async function handleClick(e) {
+    e.preventDefault();
+    const { url, options } = PHOTO_DELETE(id);
+    const { response } = await request(url, options);
+    if (response.ok) {
+      window.location.reload();
+    }
+  }
+
   return (
     <div>
-      <button className={styles.delete}>Deletar</button>
+      <button onClick={handleClick} className={styles.delete}>
+        Deletar
+      </button>
     </div>
   );
 }
