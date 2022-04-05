3f5b630c985fd54f01986e71b5f513899a097fab
Tuag <igortuag@gmail.com>
2022-04-05 07:21:53 -0300

    use component Image

--- src/components/Feed/FeedPhotosItem.js
+++ src/components/Feed/FeedPhotosItem.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Image from "../Helper/Image";
 
 import styles from "./FeedPhotosItem.module.css";
 
 function FeedPhotosItem({ photo, setModalPhoto }) {
   return (
     <li className={styles.photo} onClick={() => setModalPhoto(photo)}>
-      <img src={photo.src} alt={photo.title} />
+      <Image src={photo.src} alt={photo.title} />
       <span className={styles.view}>{photo.acessos}</span>
     </li>
   );
