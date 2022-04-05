12bc5bbd064133ec6354ea788a0d0ced80765d91
Tuag <igortuag@gmail.com>
2022-04-05 07:20:46 -0300

    add props to image

--- src/components/Helper/Image.js
+++ src/components/Helper/Image.js
@@ -1,8 +1,13 @@
 import React from "react";
 import styles from "./Image.module.css";
 
-function Image() {
-  return <div className={styles.wrapper}>Image</div>;
+function Image({ alt, ...props }) {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.skeleton}></div>
+      <img className={styles.img} alt={alt} {...props} />
+    </div>
+  );
 }
 
 export default Image;
