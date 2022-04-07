3102eb02bcabd88af00044db0a6dcd113c8700e9
Tuag <igortuag@gmail.com>
2022-04-07 07:43:10 -0300

    pass user id to feed on user page

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -8,11 +8,13 @@ import UserPhotoPost from "./UserPhotoPost";
 import UserStats from "./UserStats";
 
 const User = () => {
+  const { data } = React.useContext(UserContext);
+
   return (
     <section className="container">
       <UserHeader />
       <Routes>
-        <Route path="/" element={<Feed />} />
+        <Route path="/" element={<Feed user={data.id} />} />
         <Route path="/post" element={<UserPhotoPost />} />
         <Route path="/stats" element={<UserStats />} />
       </Routes>
