501c9dbea6a612a5cf47c4d25d1f1e7142cb60e9
Tuag <igortuag@gmail.com>
2022-04-07 07:47:06 -0300

    add event listner to a function infiniteScrool

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -5,6 +5,20 @@ import FeedPhotos from "./FeedPhotos";
 const Feed = ({ user }) => {
   const [modalPhoto, setModalPhoto] = React.useState(null);
 
+  React.useEffect(() => {
+    function infiniteScroll(event) {
+      console.log(event);
+    }
+
+    window.addEventListener("wheel", infiniteScroll);
+    window.addEventListener("scroll", infiniteScroll);
+
+    return () => {
+      window.removeEventListener("wheel", infiniteScroll);
+      window.removeEventListener("scroll", infiniteScroll);
+    };
+  }, []);
+
   return (
     <div>
       {modalPhoto && (
