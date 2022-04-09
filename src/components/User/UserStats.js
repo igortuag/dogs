b71585fba4e2b604831b94b64de8ceaf1a64bf95
Tuag <igortuag@gmail.com>
2022-04-09 07:32:19 -0300

    add useFetch to stats

--- src/components/User/UserStats.js
+++ src/components/User/UserStats.js
@@ -1,6 +1,10 @@
 import React from "react";
 
+import useFetch from "../../hooks/useFetch";
+
 const UserStats = () => {
+  const { data, error, loading, request } = useFetch();
+
   return (
     <div>
       <h1>UserStats</h1>
