0e4dad6c97c979646975cd6cc7caa08d3fd77713
Tuag <igortuag@gmail.com>
2022-04-10 07:48:39 -0300

    add error and loading if exist

--- src/components/Photo/Photo.js
+++ src/components/Photo/Photo.js
@@ -2,6 +2,8 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { PHOTO_GET } from "../../api";
 import useFetch from "../../Hooks/useFetch";
+import Error from "../Helper/Error";
+import Loading from "../Helper/Loading";
 
 function Photo() {
   const { id } = useParams();
@@ -11,6 +13,10 @@ function Photo() {
     const { url, options } = PHOTO_GET(id);
     request(url, options);
   }, [id, request]);
+
+  if (error) return <Error error={error} />;
+
+  if (loading) return <Loading />;
 
   return <div>{id}</div>;
 }
