9578b64dd2e54e5119abdbfb9ff85b7da50f80f6
Tuag <igortuag@gmail.com>
2022-04-10 07:45:57 -0300

    add id by use params

--- src/components/Photo/Photo.js
+++ src/components/Photo/Photo.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 function Photo() {
-  return <div>Photo</div>;
+  const { id } = useParams();
+
+  return <div>{id}</div>;
 }
 
 export default Photo;
