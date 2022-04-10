83c99e9d4003b3e913ff46372fafd940a4a496b1
Tuag <igortuag@gmail.com>
2022-04-10 07:47:58 -0300

    add effect to fetching options details

--- src/components/Photo/Photo.js
+++ src/components/Photo/Photo.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { PHOTO_GET } from "../../api";
+import useFetch from "../../Hooks/useFetch";
 
 function Photo() {
   const { id } = useParams();
+  const { data, loading, error, request } = useFetch();
+
+  React.useEffect(() => {
+    const { url, options } = PHOTO_GET(id);
+    request(url, options);
+  }, [id, request]);
 
   return <div>{id}</div>;
 }
