9e4e34d954f3aa4cfe1229c8858c69219df6493c
Tuag <igortuag@gmail.com>
2022-04-11 07:09:38 -0300

    add feed to user profile

--- src/components/User/UserProfile.js
+++ src/components/User/UserProfile.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import Feed from "../Feed/Feed";
 
 function UserProfile() {
-  return <div>UserProfile</div>;
+  const { user } = useParams();
+
+  return (
+    <div>
+      <Feed user={user} />
+    </div>
+  );
 }
 
 export default UserProfile;
