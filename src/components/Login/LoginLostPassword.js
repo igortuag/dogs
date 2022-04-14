8f533fcf1d3af73557a991277844fe90e7b5f784
Tuag <igortuag@gmail.com>
2022-04-14 07:50:49 -0300

    add request new password

--- src/components/Login/LoginLostPassword.js
+++ src/components/Login/LoginLostPassword.js
@@ -2,19 +2,28 @@ import React from "react";
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
 import useForm from "../../Hooks/useForm";
+import useFetch from "../../Hooks/useFetch";
+import { PASSWORD_LOST } from "../../api";
 
 const LoginLostPassword = () => {
-  const email = useForm("");
+  const login = useForm("");
+  const { data, loading, error, request } = useFetch();
 
   const handleSubmit = (event) => {
     event.preventDefault();
+
+    const { url, options } = PASSWORD_LOST({
+      login: login.value,
+      url: window.location.href.replace("forgot", "resetar"),
+    });
+    request(url, options);
   };
 
   return (
     <section>
       <h1 className="title">Perdeu a senha?</h1>
       <form onSubmit={handleSubmit}>
-        <Input label="Email / Usuário" type="text" name="email" {...email} />
+        <Input label="Email / Usuário" type="text" name="email" {...login} />
         <Button type="submit">Enviar Email</Button>
       </form>
     </section>
