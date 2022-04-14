c44a1ea243fa254b23f9671a27ab0147eb90be04
Tuag <igortuag@gmail.com>
2022-04-14 07:46:10 -0300

    add form to forgot password

--- src/components/Login/LoginLostPassword.js
+++ src/components/Login/LoginLostPassword.js
@@ -1,9 +1,22 @@
 import React from "react";
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
+import useForm from "../../Hooks/useForm";
 
 const LoginLostPassword = () => {
+  const email = useForm("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <section>
       <h1 className="title">Perdeu a senha?</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="Email / Usuário" type="text" name="email" {...email} />
+        <Button type="submit">Enviar Email</Button>
+      </form>
     </section>
   );
 };
