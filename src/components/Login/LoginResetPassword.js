507ba08fa2cc21836a3ca23a91826269d8047fbd
Tuag <igortuag@gmail.com>
2022-04-15 07:29:29 -0300

    add form to  request reset password

--- src/components/Login/LoginResetPassword.js
+++ src/components/Login/LoginResetPassword.js
@@ -1,8 +1,15 @@
 import React from "react";
+import Button from "../Forms/Button";
+import Input from "../Forms/Input";
+import useForm from "../../Hooks/useForm";
+import useFetch from "../../Hooks/useFetch";
+import { PASSWORD_RESET } from "../../api";
 
 const LoginResetPassword = () => {
   const [login, setLogin] = React.useState("");
   const [key, setKey] = React.useState("");
+  const password = useForm();
+  const { data, loading, error, request } = useFetch();
 
   React.useEffect(() => {
     const params = new URLSearchParams(window.location.search);
@@ -14,7 +21,32 @@ const LoginResetPassword = () => {
     }
   }, []);
 
-  return <div>LoginResetPassword</div>;
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (password.validate()) {
+      const { url, options } = PASSWORD_RESET({
+        login,
+        key,
+        password: password.value,
+      });
+      request(url, options);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <Input
+          label="Nova Senha"
+          type="password"
+          name="password"
+          {...password}
+        />
+        <Button>Resetar</Button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginResetPassword;
