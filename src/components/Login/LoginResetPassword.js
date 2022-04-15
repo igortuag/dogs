0f1ad7195708adb5d70820903a7faa77610b7451
Tuag <igortuag@gmail.com>
2022-04-15 07:25:02 -0300

    add states to logion and key

--- src/components/Login/LoginResetPassword.js
+++ src/components/Login/LoginResetPassword.js
@@ -1,6 +1,19 @@
 import React from "react";
 
 const LoginResetPassword = () => {
+  const [login, setLogin] = React.useState("");
+  const [key, setKey] = React.useState("");
+
+  React.useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const login = params.get("login");
+    const key = params.get("key");
+    if (login && key) {
+      setLogin(login);
+      setKey(key);
+    }
+  }, []);
+
   return <div>LoginResetPassword</div>;
 };
 
