a8e5e2b4e306aa75a8dc667f280597956cb13230
Tuag <igortuag@gmail.com>
2022-04-16 07:27:22 -0300

    add description to the page

--- src/components/Helper/Head.js
+++ src/components/Helper/Head.js
@@ -3,6 +3,9 @@ import React from "react";
 function Head(props) {
   React.useEffect(() => {
     document.title = props.title + "| Dogs";
+    document
+      .querySelector("meta[name=description]")
+      .setAttribute("content", props.description || "");
   }, [props]);
 
   return <></>;
