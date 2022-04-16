18b4f3590d3ba71110332ecc4910386599513157
Tuag <igortuag@gmail.com>
2022-04-16 07:29:41 -0300

    add head to user profile

--- src/components/Helper/Head.js
+++ src/components/Helper/Head.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Head(props) {
   React.useEffect(() => {
-    document.title = props.title + "| Dogs";
+    document.title = props.title + " | Dogs";
     document
       .querySelector("meta[name=description]")
       .setAttribute("content", props.description || "");
