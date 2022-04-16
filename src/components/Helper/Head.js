70ccfb40b7d91edbe8ba6d0f8eaa67f752ee466b
Tuag <igortuag@gmail.com>
2022-04-16 07:25:59 -0300

    change page head

--- src/components/Helper/Head.js
+++ src/components/Helper/Head.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-function Head() {
-  return <div>Head</div>;
+function Head(props) {
+  React.useEffect(() => {
+    document.title = props.title;
+  }, [props]);
+
+  return <></>;
 }
 
 export default Head;
