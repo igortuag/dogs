6167585fa83539307538b6bc76a5d6f5f71feeeb
Tuag <igortuag@gmail.com>
2022-04-17 07:21:09 -0300

    add copy right

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,11 @@ import React from "react";
 import styles from "./Footer.module.css";
 
 const Footer = () => {
-  return <footer className={styles.footer}>Footer</footer>;
+  return (
+    <footer className={styles.footer}>
+      <p>Dogs. Alguns direitos reservados.</p>
+    </footer>
+  );
 };
 
 export default Footer;
