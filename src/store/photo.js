6fdd5a37bdd8c40f9e9a3bb79f95b47eafd7bb75
Tuag <igortuag@gmail.com>
2022-04-22 07:40:49 -0300

    add thunk to fetch photo

--- src/store/photo.js
+++ src/store/photo.js
@@ -1,3 +1,5 @@
+import { PHOTO_GET } from "../api";
+
 const FETCH_PHOTO_STARTED = "photo/fetchStarted";
 const FETCH_PHOTO_SUCCESS = "photo/fetchSuccess";
 const FETCH_PHOTO_ERROR = "photo/fetchError";
@@ -49,3 +51,16 @@ export default function photo(state = initialState, action) {
       return state;
   }
 }
+
+export const fetchPhoto = (id) => async (dispatch) => {
+  try {
+    dispatch(fetchPhotoStarted());
+    const { url, options } = PHOTO_GET(id);
+    const response = await fetch(url, options);
+    const data = await response.json();
+    if (response.ok === false) throw new Error(data.message);
+    dispatch(fetchPhotoSuccess(data));
+  } catch (error) {
+    dispatch(fetchPhotoError(error.message));
+  }
+};
