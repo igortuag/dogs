2e237e2a679c7699b9442126bb08e6fdce229fff
Tuag <igortuag@gmail.com>
2022-04-22 07:46:54 -0300

    add redux photo store to modal

--- src/components/Feed/FeedModal.js
+++ src/components/Feed/FeedModal.js
@@ -1,19 +1,18 @@
 import React from "react";
 import styles from "./FeedModal.module.css";
-import useFetch from "../../Hooks/useFetch";
 import Error from "../Helper/Error";
 import Loading from "../Helper/Loading";
-import { PHOTO_GET } from "../../api";
 import PhotoContent from "../Photo/PhotoContent";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPhoto } from "../../store/photo";
 
 function FeedModal({ photo, setModalPhoto }) {
-  const { data, error, loading, request } = useFetch();
+  const { data, loading, error } = useSelector((state) => state.photo);
+  const dispatch = useDispatch();
 
   React.useEffect(() => {
-    const { url, options } = PHOTO_GET(photo?.id);
-
-    request(url, options);
-  }, [photo, request]);
+    dispatch(fetchPhoto(photo?.id));
+  }, [dispatch, photo?.id]);
 
   function handleOutsideClick(e) {
     if (e.target === e.currentTarget) {
@@ -25,7 +24,7 @@ function FeedModal({ photo, setModalPhoto }) {
     <div className={styles.modal} onClick={handleOutsideClick}>
       {error && <Error error={error} />}
       {loading && <Loading />}
-      {data && <PhotoContent data={data} />}
+      {data && <PhotoContent />}
     </div>
   );
 }
