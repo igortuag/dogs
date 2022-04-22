4bb77d995e48e1d9bb25148d3166b0b20e671188
Tuag <igortuag@gmail.com>
2022-04-22 07:35:27 -0300

    add functions return consts

--- src/store/photo.js
+++ src/store/photo.js
@@ -1,3 +1,17 @@
 const FETCH_PHOTO_STARTED = "photo/fetchStarted";
 const FETCH_PHOTO_SUCCESS = "photo/fetchSuccess";
 const FETCH_PHOTO_ERROR = "photo/fetchError";
+
+const fetchPhotoStarted = () => ({
+  type: FETCH_PHOTO_STARTED,
+});
+
+const fetchPhotoSuccess = (photo) => ({
+  type: FETCH_PHOTO_SUCCESS,
+  payload: photo,
+});
+
+const fetchPhotoError = (error) => ({
+  type: FETCH_PHOTO_ERROR,
+  payload: error,
+});
