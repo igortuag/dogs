3c04baad87f8042691bf77fb0a2caab00b051fa8
Tuag <igortuag@gmail.com>
2022-04-23 07:28:19 -0300

    add user state to login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import { UserContext } from "../../UserContext";
 
 import LoginCreate from "./LoginCreate";
 import LoginForm from "./LoginForm";
@@ -8,11 +7,12 @@ import LoginLostPassword from "./LoginLostPassword";
 import LoginResetPassword from "./LoginResetPassword";
 import styles from "./Login.module.css";
 import NotFound from "../NotFound";
+import { useSelector } from "react-redux";
 
 const Login = () => {
-  const { login } = React.useContext(UserContext);
+  const { data } = useSelector((state) => state.user);
 
-  if (login) {
+  if (data) {
     <Navigate to="/user" />;
   }
 
