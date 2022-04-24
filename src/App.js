6e81bf67e6e3f5dc5c25bbfbb168974c998959d7
Tuag <igortuag@gmail.com>
2022-04-24 08:49:44 -0300

    remove user context

--- src/App.js
+++ src/App.js
@@ -15,20 +15,18 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <UserStorage>
-          <Header />
-          <main className="AppBody">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="login/*" element={<Login />} />
-              <Route path="user/*" element={<User />} />
-              <Route path="photo/:id" element={<Photo />} />
-              <Route path="profile/:user" element={<UserProfile />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </main>
-          <Footer />
-        </UserStorage>
+        <Header />
+        <main className="AppBody">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="login/*" element={<Login />} />
+            <Route path="user/*" element={<User />} />
+            <Route path="photo/:id" element={<Photo />} />
+            <Route path="profile/:user" element={<UserProfile />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
       </BrowserRouter>
     </div>
   );
