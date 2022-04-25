72c022a27b0c6f4e0a051380ac8ea9d7960a4d8e
Tuag <igortuag@gmail.com>
2022-04-25 07:39:35 -0300

    add feed to configureStore

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -6,9 +6,10 @@ import {
 import photo from "./photo";
 import token from "./token";
 import user from "./user";
+import feed from "./feed";
 
 const middleware = [...getDefaultMiddleware()];
-const reducer = combineReducers({ photo, token, user });
+const reducer = combineReducers({ photo, token, user, feed });
 const store = configureStore({ reducer, middleware });
 
 export default store;
