86ca660135ac0832aafdc1df55fa57e1febf5d23
Tuag <igortuag@gmail.com>
2022-04-26 07:12:29 -0300

    add store feed to feed

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -2,20 +2,23 @@ import React from "react";
 import FeedModal from "./FeedModal";
 import FeedPhotos from "./FeedPhotos";
 import PropTypes from "prop-types";
+import { useDispatch, useSelector } from "react-redux";
+import { loadNewPhotos } from "../../store/feed";
 
 const Feed = ({ user }) => {
   const [modalPhoto, setModalPhoto] = React.useState(null);
-  const [pages, setPages] = React.useState([1]);
-  const [infinite, setInfinite] = React.useState(true);
+  const { infinite, loading, list, error } = useSelector((state) => state.feed);
+  const dispatch = useDispatch();
 
   React.useEffect(() => {
+    dispatch(loadNewPhotos({ user, total: 6 }));
     function infiniteScroll() {
       if (infinite) {
         let wait = false;
         const scroll = window.scrollY;
         const height = document.body.offsetHeight - window.innerHeight;
         if (scroll > height * 0.75 && !wait) {
-          setPages((prev) => [...prev, prev.length + 1]);
+          dispatch(loadNewPhotos({ user, total: 6 }));
           wait = true;
           setTimeout(() => {
             wait = false;
@@ -31,22 +34,14 @@ const Feed = ({ user }) => {
       window.removeEventListener("wheel", infiniteScroll);
       window.removeEventListener("scroll", infiniteScroll);
     };
-  }, [infinite]);
+  }, [infinite, dispatch, user]);
 
   return (
     <div>
       {modalPhoto && (
         <FeedModal setModalPhoto={setModalPhoto} photo={modalPhoto} />
       )}
-      {pages.map((page) => (
-        <FeedPhotos
-          key={page}
-          user={user}
-          page={page}
-          setModalPhoto={setModalPhoto}
-          setInfinite={setInfinite}
-        />
-      ))}
+      <FeedPhotos setModalPhoto={setModalPhoto} />
     </div>
   );
 };
