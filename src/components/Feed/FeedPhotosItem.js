c8c6fff2c26ff312ec76fc311522e1039f9ef8ac
Tuag <igortuag@gmail.com>
2022-04-27 06:55:21 -0300

    add dispatcher on feed photo to open modal

--- src/components/Feed/FeedPhotosItem.js
+++ src/components/Feed/FeedPhotosItem.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { fetchPhoto } from "../../store/photo";
+import { openModal } from "../../store/ui";
 import Image from "../Helper/Image";
 
 import styles from "./FeedPhotosItem.module.css";
 
-function FeedPhotosItem({ photo, setModalPhoto }) {
+function FeedPhotosItem({ photo }) {
+  const dispatch = useDispatch();
+
+  function handleClick() {
+    dispatch(openModal());
+    dispatch(fetchPhoto(photo.id));
+  }
+
   return (
-    <li className={styles.photo} onClick={() => setModalPhoto(photo)}>
+    <li className={styles.photo} onClick={handleClick}>
       <Image src={photo.src} alt={photo.title} />
       <span className={styles.view}>{photo.acessos}</span>
     </li>
