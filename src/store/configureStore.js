024ed7c74a4e6f01387fae13f275d35d69b193d5
Tuag <igortuag@gmail.com>
2022-04-27 06:50:54 -0300

    add ui reducer to store config

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -7,9 +7,10 @@ import photo from "./photo";
 import token from "./token";
 import user from "./user";
 import feed from "./feed";
+import ui from "./ui";
 
 const middleware = [...getDefaultMiddleware()];
-const reducer = combineReducers({ photo, token, user, feed });
+const reducer = combineReducers({ photo, token, user, feed, ui });
 const store = configureStore({ reducer, middleware });
 
 export default store;
