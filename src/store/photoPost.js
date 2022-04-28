4b4c1b7f878b76095e0e97dcb2ea2954a417a17d
Tuag <igortuag@gmail.com>
2022-04-28 07:21:28 -0300

    add photoPost structure reducer

---
+++ src/store/photoPost.js
@@ -0,0 +1,7 @@
+import { PHOTO_POST } from "../api";
+import createAsyncSlice from "./helper/createAsyncSlice";
+
+const slice = createAsyncSlice({
+  name: "photoPost",
+  fetchConfig: ({ formData, token }) => PHOTO_POST({ formData, token }),
+});
