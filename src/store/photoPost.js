d79b5c588341948b4056a5a41711158bab8d420f
Tuag <igortuag@gmail.com>
2022-04-28 07:22:10 -0300

    export photoPost reducer

--- src/store/photoPost.js
+++ src/store/photoPost.js
@@ -5,3 +5,7 @@ const slice = createAsyncSlice({
   name: "photoPost",
   fetchConfig: ({ formData, token }) => PHOTO_POST({ formData, token }),
 });
+
+export const photoPost = slice.asyncAction;
+
+export default slice.reducer;
